827629dbd36f56511d2948ccc0855b4dd3df3f7b
ARA Array <ara-c@ara.icecube.wisc.edu>
2012-02-01 15:32:13 +0000

    Updated utility scripts for TestBed and Station 1

---
+++ utilities/Icrr/runAraTestBedRunFileMaker.sh
@@ -0,0 +1,73 @@
+#/bin/bash
+if [ "$1" = "" ]
+then
+   echo "usage: `basename $0` <run num>" 1>&2
+   exit 1
+fi
+
+
+#Only need to edit these two lines to point to the local directories 
+RAW_BASE_DIR=~/ara/data/ara_station1_ICRR_calibration/event
+ROOT_BASE_DIR=~/ara/data/ara_station1_ICRR_calibration/root/Station1Test
+
+
+RUN_NUM=$1
+RUN_WITH_ZEROES=`printf %06d $RUN_NUM`
+echo $RUN_NUM $RUN_WITH_ZEROES
+
+RAW_DIR=${RAW_BASE_DIR}/run_${RUN_WITH_ZEROES}
+ROOT_DIR=${ROOT_BASE_DIR}/run${RUN_NUM}
+EVENT_FILE=${ROOT_DIR}/event${RUN_NUM}.root
+HK_FILE=${ROOT_DIR}/hk${RUN_NUM}.root
+#exit 1
+#echo ${RAW_DIR}
+
+if [[ -d $ROOT_DIR ]]; then
+    echo "Output dir exists"
+else
+    mkdir ${ROOT_DIR}
+fi
+
+echo "Starting Event File"
+EVENT_FILE_LIST=`mktemp event.XXXX`
+for file in ${RAW_DIR}/ev_*/*; 
+  do
+  if [[ -f $file ]]; then
+      echo $file >> ${EVENT_FILE_LIST}
+#      echo `dirname $file`;
+  fi
+done
+
+if  test `cat ${EVENT_FILE_LIST} | wc -l` -gt 0 ; then
+    ./makeAraEventTree ${EVENT_FILE_LIST} ${EVENT_FILE} 0 # 0 - stationId for TestBed
+#    cat ${EVENT_FILE_LIST}
+    rm ${EVENT_FILE_LIST}
+    echo "Done Event File"
+else
+    rm ${EVENT_FILE_LIST}
+    echo "No event files"
+fi
+
+
+echo "Starting Hk File"
+HK_FILE_LIST=`mktemp hk.XXXX`
+for file in ${RAW_DIR}/hk_*/*; 
+  do
+  if [[ -f $file ]]; then
+      echo $file >> ${HK_FILE_LIST}
+#      echo `dirname $file`;
+  fi
+done
+
+if  test `cat ${HK_FILE_LIST} | wc -l` -gt 0 ; then
+    ./makeAraHkTree ${HK_FILE_LIST} ${HK_FILE}
+#    cat ${HK_FILE_LIST}
+    rm ${HK_FILE_LIST}
+    echo "Done Hk File"
+else
+    rm ${HK_FILE_LIST}
+    echo "No hk files"
+fi
+
+
+
---
+++ utilities/Icrr/runAraTestBedTimeFileMaker.sh
@@ -0,0 +1,38 @@
+#/bin/bash
+if [ "$1" = "" ]
+then
+   echo "usage: `basename $0` <time num>" 1>&2
+   exit 1
+fi
+
+TIME_NUM=$1
+RAW_BASE_DIR=/unix/anita1/ara/data/pole11
+ROOT_BASE_DIR=/unix/anita1/ara/data/pole11/root
+
+if [[ -d $ROOT_BASE_DIR ]]; then
+    echo "Output dir exists"
+else
+    mkdir ${ROOT_BASE_DIR}
+fi
+
+echo "Starting Event File"
+EVENT_FILE_LIST=`mktemp`
+for file in ${RAW_BASE_DIR}/event/ev_${TIME_NUM}/*; 
+  do
+  if [[ -f $file ]]; then
+      echo $file >> ${EVENT_FILE_LIST}
+  fi
+done
+
+if  test `cat ${EVENT_FILE_LIST} | wc -l` -gt 0 ; then
+    ./makeAraEventTree ${EVENT_FILE_LIST} ${ROOT_BASE_DIR}/event_${TIME_NUM}.root 0 # 0 - stationId for TestBed
+#    cat ${EVENT_FILE_LIST}
+    rm ${EVENT_FILE_LIST}
+    echo "Done Event File"
+else
+    rm ${EVENT_FILE_LIST}
+    echo "No event files"
+fi
+
+
+
