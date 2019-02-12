75d5c607e97bb09185eab7252c279ff4fc9247bb
Brian Clark <clark.2668@osu.edu>
2019-02-12 01:32:27 +0000

    Update runAraDisplay macro to be more general

--- AraDisplay/macros/runAraDisplay.C
+++ AraDisplay/macros/runAraDisplay.C
@@ -1,40 +1,33 @@
+/*
+runAraDisplay.C
+By: Jonathan Davies
+Updated: Brian Clark (Feb 2019)
+
+This is meant to be used as a ROOT macro. For example:
+
+1) Open ROOT: `root` (with x-forwarding enabled)
+2) Load the macro: `.L runAraDisplay.C`
+3) Run the function: `runAraDisplay("/path/to/data/event1407.root", "/path/to/pedestal/pedestalValues.txt", 2);
+
+*/
+
+
 gSystem->Reset();
+void runAraDisplay(char *eventFile, char *pedFile, int stationID) {
+    gSystem->AddIncludePath("-I${ARA_UTIL_INSTALL_DIR}/include");
+    gSystem->SetDynamicPath(std::string(std::string(gSystem->GetDynamicPath())+":${ARA_UTIL_INSTALL_DIR}/lib").c_str());
+    gSystem->Load("libfftw3.so");
+    gSystem->Load("libgsl.so");
+    gSystem->Load("libMathMore.so");
+    gSystem->Load("libGeom.so");;
+    gSystem->Load("libGraf3d.so");
+    gSystem->Load("libPhysics.so");  
+    gSystem->Load("libRootFftwWrapper.so");
+    gSystem->Load("libAraEvent.so");
+    gSystem->Load("libAraCorrelator.so");
+    gSystem->Load("libAraDisplay.so");
 
-void runAraDisplay(int run, int ped) {
-  
-  char fileName[180];  
-  sprintf(fileName, "/Users/jdavies/ara/data/ntu2012/StationTwo/root/run%i/event%i.root", run, run);
-  char pedName[180];  
-  sprintf(pedName, "/Users/jdavies/ara/data/ntu2012/StationTwo/raw_data/run_%06d/pedestalValues.run%06d.dat", ped, ped);
-
-  runAraDisplay(fileName, pedName);
-
-}
-
- 
-void runAraDisplay(char *eventFile, char *pedFile) {
-  gSystem->AddIncludePath("-I${ARA_UTIL_INSTALL_DIR}/include");
-  //  gSystem->AddIncludePath("-I/sw/include");
-  gSystem->SetDynamicPath(std::string(std::string(gSystem->GetDynamicPath())+":${ARA_UTIL_INSTALL_DIR}/lib").c_str());
-  //  gSystem->Load("~/repositories/InstallDir/utilities/lib/libsqlite3.so");
-  //  gSystem->Load("libsqlite3.so");
-  gSystem->Load("libfftw3.so");
-  gSystem->Load("libgsl.so");
-  gSystem->Load("libMathMore.so");
-  gSystem->Load("libGeom.so");;
-  gSystem->Load("libGraf3d.so");
-  gSystem->Load("libPhysics.so");  
-  gSystem->Load("libRootFftwWrapper.so");     	  
-  gSystem->Load("libAraEvent.so");   	      	  
-  gSystem->Load("libAraCorrelator.so");   	  
-  gSystem->Load("libAraDisplay.so");
-
-  TChain *fred=0; //Will this work?
-
-  AraEventCalibrator::Instance()->setAtriPedFile(pedFile,2);
-
-  AraDisplay *magicPtr = new AraDisplay(eventFile,AraCalType::kJustPed);  
-  //  AraDisplay *magicPtr = new AraDisplay(eventFile,AraCalType::kNoCalib);  
-
-  magicPtr->startEventDisplay();
+    AraEventCalibrator::Instance()->setAtriPedFile(pedFile,2);
+    AraDisplay *magicPtr = new AraDisplay(eventFile,AraCalType::kJustPed);
+    magicPtr->startEventDisplay();
 }
