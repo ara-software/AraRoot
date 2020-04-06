17e3e5df7b177c4d22dee0b40a5b43651e244174
clark2668 <baclark@msu.edu>
2020-04-06 10:11:58 -0500

    identation should be done with spaces, oops

--- AraVertex/AraRecoHandler.h
+++ AraVertex/AraRecoHandler.h
@@ -9,17 +9,17 @@ using namespace std;
 #define ARARECOHANDLER_H
 
 class AraRecoHandler {
-	public:
-		AraRecoHandler();
-		~AraRecoHandler();
+    public:
+        AraRecoHandler();
+        ~AraRecoHandler();
 
-		// some functions for dealing with SNR calculation
-		int getMaxBin(TGraph *gr);
-		void setMeanAndSigmaInNoMax(TGraph *gr, double *stats);
-		TGraph* getSqrtVoltageSquaredSummedWaveform(TGraph *gr, int nIntSamp);
-		void getChannelSlidingV2SNR_UW(vector<TGraph*> interpolatedWaveforms, int nIntSamp_V, int nIntSamp_H, float *snrArray, float *hitTimeArray);
-		
-		// a function for hit finding and preparing to vertex
-		void identifyHitsPrepToVertex(AraGeomTool *araGeom, AraVertex *Reco, int station, int pol_select, vector<int> excluded_channels, vector<TGraph*> waveforms, double hitThreshold=8.);
+        // some functions for dealing with SNR calculation
+        int getMaxBin(TGraph *gr);
+        void setMeanAndSigmaInNoMax(TGraph *gr, double *stats);
+        TGraph* getSqrtVoltageSquaredSummedWaveform(TGraph *gr, int nIntSamp);
+        void getChannelSlidingV2SNR_UW(vector<TGraph*> interpolatedWaveforms, int nIntSamp_V, int nIntSamp_H, float *snrArray, float *hitTimeArray);
+        
+        // a function for hit finding and preparing to vertex
+        void identifyHitsPrepToVertex(AraGeomTool *araGeom, AraVertex *Reco, int station, int pol_select, vector<int> excluded_channels, vector<TGraph*> waveforms, double hitThreshold=8.);
 };
 #endif
