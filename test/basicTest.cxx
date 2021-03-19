11090c76de4a5e51687bb40eadaf846958192225
Brian Clark <baclark@msu.edu>
2021-03-19 14:04:28 -0500

    Add a first attempt at a basic test, just to make sure things run

---
+++ test/basicTest.cxx
@@ -0,0 +1,14 @@
+#include "TFile.h"
+#include "RawAtriStationEvent.h"
+#include "UsefulAtriStationEvent.h"
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+
+  std::cout<<"Hello world!"<<std::endl;
+
+}
+
